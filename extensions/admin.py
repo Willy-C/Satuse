bfc509f08d155d15b0f0625456921e463356dd37
Willy <19799671+Willy-C@users.noreply.github.com>
2023-03-06 15:25:16 -0500

    Add logs command to send latest logs to discord

--- extensions/admin.py
+++ extensions/admin.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+import pathlib
 from typing import TYPE_CHECKING
 
 import discord
 from discord.ext import commands
+
+from config import SERVER_DIR
 
 if TYPE_CHECKING:
     from main import Bot
@@ -13,8 +16,12 @@ class Admin(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
+    async def cog_check(self, ctx: commands.Context):
+        if not await self.bot.is_owner(ctx.author):
+            raise commands.NotOwner
+        return True
+
     @commands.command()
-    @commands.is_owner()
     async def status(self, ctx: commands.Context, status: bool):
         """Change status of server"""
         if status:
@@ -32,10 +39,23 @@ class Admin(commands.Cog):
             await ctx.reply('Server status set to OFF')
 
     @commands.command(name='shutdown')
-    @commands.is_owner()
     async def shutdown_bot(self, ctx: commands.Context):
         await ctx.message.add_reaction('\U0001f620')
         await ctx.bot.close()
+
+    @commands.command(name='logs')
+    @commands.is_owner()
+    async def logs(self, ctx: commands.Context):
+        cmd = self.bot.get_command('jsk cat')
+        if cmd is None:
+            return await ctx.reply('Command not found', mention_author=False)
+
+        log_file = pathlib.Path(SERVER_DIR) / 'logs' / 'latest.log'
+        if not log_file.is_file():
+            return await ctx.reply('Log file not found', mention_author=False)
+
+        await ctx.invoke(cmd, str(log_file)) # type: ignore
+
 
 async def setup(bot: Bot):
     await bot.add_cog(Admin(bot))
